fcc5fd74af0c21d387809fc802118f0379331cc3
Rasto <rast3580@gmail.com>
2026-03-26 20:17:08 +0300

    commit

--- master/server/src/index.ts
+++ master/server/src/index.ts
@@ -20,7 +20,7 @@ app.use('/api/schools', requireMasterAuth, schoolsRouter);
 // Serve built client in production
 const clientDist = path.join(__dirname, '../../client/dist');
 app.use(express.static(clientDist));
-app.get('*', (_req, res) => {
+app.get('/{*splat}', (_req, res) => {
   res.sendFile(path.join(clientDist, 'index.html'));
 });
 
